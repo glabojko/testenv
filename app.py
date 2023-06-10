38fe366454daaa253e58fbe2e3d3b65f68f64748
glabojko <glabojko@gmail.com>
2023-06-10 23:56:12 +0200

    trying to connect to database

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import data_manager
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
+
     return render_template('index.html')
 
 @app.route("/child", methods=["POST", "GET"])
 def child():
-    return render_template('child.html')
+    if request.method == 'POST':
+        id = request.form.get('id')
+        name = request.form.get('name')
+        pesel = request.form.get('pesel')
+    data_manager.add_child_to_database(id, name, pesel)
+    return render_template('child.html', id=id, name=name, pesel=pesel)
 
 if __name__ == '__main__':
     app()
--- data_manager.py
+++ data_manager.py
@@ -2,9 +2,9 @@ import database
 
 
 @database.connection_handler
-def add_new_child(cursor, name:str, pesel:int):
+def add_child_to_database(cursor, id:str, name:str, pesel:str):
     query = """
-          INSERT INTO children (name, pesel) 
-          VALUES (%(name)s, %(pesel)s); """
-    data = {'name': name, 'pesel': pesel}
+          INSERT INTO children (id, name, pesel) 
+          VALUES (%(id)s, %(name)s, %(pesel)s); """
+    data = {'id': id, 'name': name, 'pesel': pesel}
     cursor.execute(query, data)
